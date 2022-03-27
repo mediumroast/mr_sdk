49983a6076e15a5f419f11e1291a4197c954f4c3
mihay42 <mihay42@gmail.com>
2022-03-26 17:08:13 -0700

    Pulled in relevant substudy data

--- javascript/cli/doc_company.js
+++ javascript/cli/doc_company.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Import required modules
-import { Companies, Interactions } from '../src/api/highLevel.js'
+import { Companies, Interactions, Studies } from '../src/api/highLevel.js'
 import Firmographics from '../src/report/company.js'
 import Utilities from '../src/report/common.js'
 import program from 'commander'
@@ -9,7 +9,8 @@ import ConfigParser from 'configparser'
 import * as fs from "fs"
 import docx from 'docx'
 import AWS from 'aws-sdk'
-// import zip from 'adm-zip'
+import zip from 'adm-zip'
+import boxPlot from 'box-plot'
 
 
 //  ______   __  __     __   __     ______     ______   __     ______     __   __     ______    
@@ -25,10 +26,12 @@ function parseCLIArgs() {
         .version('0.7.5')
         .description('A CLI to generate a document report for mediumroast.io Company objects.')
     program
-        .requiredOption('-g --guid <guid>', 'The GUID for the company to construct a report for.')
+        .requiredOption('-n --name <name>', 'The GUID for the company to construct a report for.')
+        .option('-s --substudy <study_name:substudy_id>', 'The GUID for the substudy to include in the report.')
         .option('-r --report_dir <directory>', 'Directory to write the report to', 'Documents')
         .option('-w --work_dir <directory>', 'Directory to use for creating a ZIP package', '~/Documents')
-        .option('-z --zip_package', 'Create a zip package with interactions')
+        .option('-p --package', 'Create a package with interactions')
+        .option('-z --zip', 'Create a ZIP archive with report and interactions')
         .option('-s --server <server>', 'Specify the server URL', 'http://mr-01:3000')
         .option('-t --server_type <type>', 'Specify the server type as [json || mr_server]', 'json')
         .option('-a --author_company <type>', 'Specify the company the report is for')
@@ -38,18 +41,42 @@ function parseCLIArgs() {
     return options
 }
 
-// Filter objects by the GUID of the Company
-function filterInteractions(objects, guid) {
-    let myInteractions = []
+// Filter interactions by the GUID of the Company
+function filterObjects(objects, guid) {
+    let myObjects = []
     for (const object in objects) {
         const allCompanies = Object.values(objects[object].linkedCompanies)
         if (allCompanies.includes(guid)) {
-            myInteractions.push(objects[object])
+            myObjects.push(objects[object])
         }
     }
-    return myInteractions
+    return myObjects
 }
 
+function rankTags (tags, ranges) {
+    let finalTags = {}
+    for (const tag in tags) {
+        // Rank the tag score using the ranges derived from box plots
+        // if > Q3 then the ranking is high
+        // if in between Q2 and Q3 then the ranking is medium
+        // if < Q3 then the ranking is low
+        let rank = null
+        if (tags[tag] > ranges.upperQuartile) {
+            rank = 'High'
+        } else if (tags[tag] < ranges.lowerQuartile) {
+            rank = 'Low'
+        } else if (ranges.lowerQuartile <= tags[tag] <= ranges.upperQuartile) {
+            rank = 'Medium'
+        }
+
+        finalTags[tag] = {
+            score: tags[tag], // Math.round(tags[tag]),
+            rank: rank
+        }
+        
+    }
+    return finalTags
+}
 
 //  ______     ______     __   __     ______   __     ______    
 // /\  ___\   /\  __ \   /\ "-.\ \   /\  ___\ /\ \   /\  ___\   
@@ -93,12 +120,21 @@ const localProtocol = 'file'
 const httpProtocol = 'http'
 
 // Determine if we need to create a zip package or not
-let createZIP = null
-opts.zip_package ? createZIP = true : createZIP = false
+let createPackage = null
+opts.package ? createPackage = true : createPackage = false
+
+// Determine if we need to create a zip package or not
+let createArchive = null
+opts.zip ? createArchive = true : createArchive = false
+
+// Obtain the GUID for the needed substudy
+let addedSubstudy = null
+opts.substudy ? addedSubstudy = opts.substudy : addedSubstudy = false
 
 // Set up the control objects
 const companyCtl = new Companies(mrServer, serverType)
 const interactionCtl = new Interactions(mrServer, serverType)
+const studyCtl = new Studies(mrServer, serverType)
 const docCtl = new Utilities(
     config.get('document_settings', 'font_type'),
     parseInt(config.get('document_settings', 'font_size')),
@@ -115,8 +151,19 @@ const s3Ctl = new AWS.S3({
 }) 
 
 // Get the company in question and all interactions
-const company = await companyCtl.getByGUID(opts.guid)
-const interactions = filterInteractions(await interactionCtl.getAll(), opts.guid)
+const company = await companyCtl.getByName(opts.name)
+const interactions = filterObjects(await interactionCtl.getAll(), opts.guid)
+
+// Get the relevant study and substudy 
+const [studyName, substudyId] = opts.substudy.split(':')
+const studies = await studyCtl.getByName(studyName)
+const substudy = studies[0].substudies[substudyId]
+const ranges = boxPlot(Object.values(substudy.keyThemes.summary_theme.tags))
+const quotes = (substudy.keyThemeQuotes.summary)
+const tags = rankTags(substudy.keyThemes.summary_theme.tags, ranges)
+// console.log(ranges)
+// console.log(tags)
+// console.log(quotes)
 
 // simple function for safe directory creation
 function safeMakedir(name) {
@@ -130,17 +177,37 @@ function safeMakedir(name) {
 }
 
 // create a ZIP package
-// NOTE Grumble grumble, this ZIPs oddly. After files written, incomplete directories, etc.
-// function createZipArchive(outputFile, sourceDirectory) {
-//     try {
-//       const zipPackage = new zip();
-//       zipPackage.addLocalFolder(sourceDirectory);
-//       zipPackage.writeZip(outputFile);
-//       console.log(`Created ${outputFile} successfully`);
-//     } catch (e) {
-//       console.log(`Something went wrong. ${e}`);
-//     }
-// }
+async function createZIPArchive
+(outputFile, sourceDirectory) {
+    try {
+      const zipPackage = new zip();
+      zipPackage.addLocalFolder(sourceDirectory);
+      zipPackage.writeZip(outputFile);
+      console.log(`Created ${outputFile} successfully`);
+    } catch (e) {
+      console.log(`Something went wrong. ${e}`);
+    }
+}
+
+function writeReport (docObj, fileName) {
+    docx.Packer.toBuffer(docObj).then((buffer) => {
+        fs.writeFileSync(fileName, buffer)
+    })
+}
+
+// Download the objects
+async function downloadInteractions (interactions, directory) {
+    for (const interaction in interactions) {
+        const objWithPath = interactions[interaction].url.split('://').pop()
+        const myObj = objWithPath.split('/').pop()
+        const myParams = {Bucket: s3Source, Key: myObj}
+        const myFile = fs.createWriteStream(directory + myObj)
+        s3Ctl.getObject(myParams).
+            on('httpData', function(chunk) { myFile.write(chunk) }).
+            on('httpDone', function() { myFile.end() }).
+            send()
+    }
+}
 
 //  __    __     ______     __     __   __        ______     __         __    
 // /\ "-./  \   /\  __ \   /\ \   /\ "-.\ \      /\  ___\   /\ \       /\ \   
@@ -175,38 +242,26 @@ let doc = new docx.Document ({
 })
 
 // If needed create the zip package
-if (createZIP) {
-    const outputPackage = outputFile + '.zip'
+if (createPackage) {
+    
     const fileName = 'Company Report.docx'
     const interactionsDir = 'Interactions/'
-    
+
     // As needed create the working directory
     const workingDirectory = workDir + '/' + company[0].companyName + '/'
     safeMakedir(workingDirectory)
     safeMakedir(workingDirectory + interactionsDir)
 
-    // Download the objects
-    for (const interaction in interactions) {
-        const objWithPath = interactions[interaction].url.split('://').pop()
-        const myObj = objWithPath.split('/').pop()
-        const myParams = {Bucket: s3Source, Key: myObj}
-        const myFile = fs.createWriteStream(workingDirectory + interactionsDir + myObj)
-        s3Ctl.getObject(myParams).
-            on('httpData', function(chunk) { myFile.write(chunk) }).
-            on('httpDone', function() { myFile.end() }).
-            send()
-    }
-
     // Write the report to the working directory
-    docx.Packer.toBuffer(doc).then((buffer) => {
-        fs.writeFileSync(workingDirectory + fileName, buffer)
-    })
+    writeReport(doc, workingDirectory + fileName)
 
-    // Create the zip package
-    // createZipArchive(outputPackage, workDir + '/' + company[0].companyName)
+    // Download the objects
+    await downloadInteractions(interactions, workingDirectory + interactionsDir)
     
+} else if (createArchive) {
+    const outputPackage = outputFile + '.zip'
+    // Create the zip package
+    await createZIPArchive(outputPackage, workDir + '/' + company[0].companyName)
 } else {
-    docx.Packer.toBuffer(doc).then((buffer) => {
-        fs.writeFileSync(outputDocFileName, buffer)
-    })
+    writeReport(doc, outputDocFileName)
 }
