20f43858f381fb1716f03dc3dba44a8b267a6055
mihay42 <mihay42@gmail.com>
2022-04-16 21:36:23 -0700

    Added initial take at discovery plus cleanups

--- python/cli/ingest.py
+++ python/cli/ingest.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os, argparse, cmd, re, magic, pyfiglet
+import os, argparse, cmd, re, magic, pyfiglet, pdfx
 
 def parse_cli_args(program_name='ingest', desc='A mediumroast.io example utility to ingest data into the backend.'):
     parser=argparse.ArgumentParser(prog=program_name, description=desc)
@@ -17,11 +17,24 @@ class IngestShell(cmd.Cmd):
     ruler = '_'
 
     def __init__(self, src_type):
-        self.authors = ['Michael Hay']
-        self.copyright = "Copyright 2022 Mediumroast, Inc. All rights reserved."
         self.src_type = src_type
-        self.env={}
+        self.companies = []
+        self.studies = []
+        self.interactions = []
+        self.env={
+            'copyright': "Copyright 2022 Mediumroast, Inc. All rights reserved.",
+            'support email': 'help@mediumroast.io',
+            'version': '1.0.0',
+            'folder': ""
+        }
         super(IngestShell, self).__init__()
+
+    #############################################################################
+    # Description: Basic reusable utility functions for the command shell
+    #
+    #
+    #
+    #############################################################################
 
     def print_help(self, help):
         print("\n" + help['Name'] + ' ' + help['Argument'] + help['Description'])
@@ -45,15 +58,36 @@ class IngestShell(cmd.Cmd):
             else: 
                 final_list.append(item)
         return final_list
+    
+    def do_print (self, obj_type):
+        obj_type = obj_type.strip().lower()
+        print('The following ' + obj_type + ' are prepared for ingestion.')
+        if obj_type == 'companies':
+            self.print_objects(self.companies)
+        elif obj_type == 'studies':
+            self.print_objects(self.studies)
+        elif obj_type == 'interactions':
+            self.print_objects(self.interactions)
+        else:
+            print("\n\tError: Unsupported object type [" + obj_type + ']. Supported types are: studies, companies or interactions.')
+
+    def help_print(self):
+        my_help = {
+            'Name': 'print',
+            'Argument': '<object type>',
+            'Description': "\n\tPrint out the current set of objects ready for ingestion, where object types are companies, studies or interactions." +
+                "\n"
+        }
+        self.print_help(help=my_help)
 
     def item_type(self, item):
-        return magic.from_file(self.folder + '/' + item) #, mime=True) 
+        return magic.from_file(self.env['folder'] + '/' + item)
     
     def decode_folder(self):
-        folder_string = "\n" + self.folder + " [folder]\n"
+        folder_string = "\n" + self.env['folder'] + " [folder]\n"
         idx = 1
         for item in self.folder_data:
-            if os.path.isdir(self.folder + '/' + item):
+            if os.path.isdir(self.env['folder'] + '/' + item):
                 folder_string+= "\t" + str(idx) +". " + item + " [dir]\n"
             else:
                 folder_string+= "\t" + str(idx) +". " + item + " [" + self.item_type(item) + "]\n" 
@@ -61,8 +95,8 @@ class IngestShell(cmd.Cmd):
         return folder_string
 
     def do_list(self, folder):
-        self.folder = folder
-        self.folder_data = self.filter_raw(os.listdir(self.folder))
+        self.env['folder'] = folder if folder else self.env['folder'] 
+        self.folder_data = self.filter_raw(os.listdir(self.env['folder']))
         print(self.decode_folder())
 
     def help_list(self):
@@ -73,9 +107,6 @@ class IngestShell(cmd.Cmd):
                 "\n"
         }
         self.print_help(help=my_help)
-
-    def do_process(self, folder):
-        pass
 
     def do_set(self, env_var):
         variable, value = env_var.split('=')
@@ -91,50 +122,168 @@ class IngestShell(cmd.Cmd):
         }
         self.print_help(help=my_help)
 
-    def help_process(self):
-        print("\nprocess [folder name]\nPerform steps needed to process an individual folder and the children.")
 
-    def print_object(self, obj):
+    def print_objects(self, obj):
+        index = 1
         for item in obj:
-            print("\t", item + ':', obj[item])
+            print('Item index: ' + str(index))
+            for attribute in item:
+                print("\t", attribute + ':', item[attribute])
+            print('-' * 30)
+            index+=1
 
-    def add_company(self):
-        my_script = [
-            'name',
-            'description',
-            'address',
-            'city',
-            'state/province',
-            'zip/postal code',
-            'country',
-            'region',
-            'phone number',
-            'website',
-            'logo URL',
-            'industry',
-            'CIK',
-            'stock symbol'
-        ]
-        my_company = {}
-        for topic in my_script:
+    #############################################################################
+    #
+    # NAME: Add Objects
+    # Description: Core logic and helpers to add objects one at a time using a prompt
+    # Syntax: add <object type>
+    # Argument: <object type> can be one of study, company or interaction
+    # Core methods: 
+    #   add study - starts a prompt based interface to add a study
+    #   print studies - prints out all added studies
+    #
+    #############################################################################
+    def _add_object(self, obj_type, obj_script):
+        my_obj = {}
+        for obj_topic in obj_script:
             default = 'Unknown'
-            print('Do you know the company\'s ' + topic + ' [Default: Unknown]?  ', end='')
+            print('Enter the '+ obj_type + '\'s ' + obj_topic + ' [Default: Unknown]?  ', end='')
             answer = input().strip()
             if not answer: answer = default
-            my_company[topic] = answer
+            my_obj[obj_script[obj_topic]] = answer # Set to the final structure for the API
+        return my_obj
 
-        print('Preparing to add company with the following attributes:')
-        self.print_object(my_company)
+    def add_study(self):
+        # TODO in the far future consider type checking for select attributes
+        my_script = {
+            'name': 'studyName',
+            'description': 'description',
+            'access privileges': 'public',
+            'accessible groups': 'groups',
+        }
+        return self._add_object('study', my_script)
 
+    def add_interaction(self):
+        # TODO in the far future consider type checking for select attributes
+        my_script = {
+            'name': 'interactionName',
+            'time': 'time',
+            'date': 'date',
+            'state': 'state',
+            'description': 'description',
+            'contact name': 'contactName',
+            'contact address': 'contactAddress',
+            'contact city': 'contactCity',
+            'contact state/province': 'contactStateProvince',
+            'contact zip/postal code': 'contactZipPostal',
+            'contact country': 'contactCountry',
+            'contact region': 'contactRegion',
+            'contact phone number': 'contactPhone',
+            'contact LinkedIn profile': 'contactLinkedIn',
+            'contact Twitter profile': 'contactTwitter',
+            'contact email address': 'contactEmail',
+            'access privileges': 'public',
+            'interaction type': 'interactionType',
+            'status': 'status',
+            'url': 'url'
+        }
+        return self._add_object('interaction',my_script)
+
+    def add_company(self):
+        # TODO in the far future consider type checking for select attributes
+        my_script = {
+            'name': 'companyName',
+            'description': 'description',
+            'address': 'address',
+            'city': 'city',
+            'state/province': 'stateProvince',
+            'zip/postal code': 'zipPostal',
+            'country': 'country',
+            'region': 'region',
+            'phone number': 'phone',
+            'website': 'url',
+            'logo URL': 'logoURL',
+            'industry': 'industry',
+            'CIK': 'cik',
+            'stock symbol': 'stockSymbol'
+        }
+        return self._add_object('company', my_script)
 
     def do_add(self, sub_command):
         sub_command = sub_command.strip().lower()
         if sub_command == 'company':
-            self.add_company()
+            self.companies.append(self.add_company())
         elif sub_command == 'study':
-            self.add_study()
+            self.studies.append(self.add_study())
         elif sub_command == 'interaction':
-            self.add_interaction()
+            self.interactions.append(self.add_interaction())
+        else:
+            print("\n\tError: Unsupported subcommand [" + sub_command + ']. Supported subcommands are: study, company or interaction.')
+
+    def help_add(self):
+        my_help = {
+            'Name': 'add',
+            'Argument': '<subcommand>',
+            'Description': "\n\tAdd one or more company, interaction or study objects by hand through a series of prompts." +
+                "\n\tSubcommand can be one of study, company or interaction." +
+                "\n"
+        }
+        self.print_help(help=my_help)
+
+
+    #############################################################################
+    #
+    # NAME: Discover Objects
+    # Description: Core logic and helpers to discover objects based upon a folder
+    # Syntax: discover <sub_folder>
+    # Argument: <sub_folder> is a child to the internally set folder in the 'env'
+    # Core methods: 
+    #   discover Competition - creates a study called 'Competition', and tries to discover companies and interactions in 'Competition'
+    #
+    #############################################################################
+
+    def _get_pdf_meta(self, item):
+        pdf = pdfx.PDFx(self.env['folder'] + '/' + item)
+        meta = pdf.get_metadata()
+        return meta
+
+    def do_discover(self, sub_folder):
+        my_study = {'studyName': sub_folder}
+        my_interactions = []
+        my_companies = []
+        self.folder_data = self.filter_raw(os.listdir(self.env['folder'] + '/' + sub_folder))
+        for item in self.folder_data:
+            doc_type = self.item_type(sub_folder + '/' + item)
+            if re.match(r'^PDF', doc_type):
+                my_meta = self._get_pdf_meta(sub_folder + '/' + item)
+                date = None
+                time = None
+                if 'xap' in my_meta:
+                    raw_date = my_meta['xap']['CreateDate']
+                    [date, time] = raw_date.split('T')
+                    date = date.replace('-', '')
+                    time = ''.join(time.split('-')[0].split(':')[0:2])
+            my_interaction = {
+                'interactionName': item.split('.')[0],
+                'time': time,
+                'date': date,
+            }
+            my_interactions.append(my_interaction)
+
+        self.interactions+=my_interactions
+
+        
+
+    def help_discover(self):
+        my_help = {
+            'Name': 'discover',
+            'Argument': '<subfolder>',
+            'Description': "\n\tDiscover a study, companies and interactions " +
+                "\n\tSubcommand can be one of study, company or interaction." +
+                "\n"
+        }
+        self.print_help(help=my_help)
+
 
 
 if __name__ == "__main__":
